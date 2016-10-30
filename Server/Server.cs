0845a607a1622a7730bdb97ed054ee2ee35a81e7
WojtekP12 <wojciech.pelka@onet.pl>
2016-10-30 18:01:59 +0100

    UDP Server implemented

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,15 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    class Program
+    class Server
     {
+        static ushort tcpPort;
+        static List<string> localIpListForTcp = new List<string>();
+        static bool isPortValid;
+
         static void Main(string[] args)
         {
+            GetIpList();
+
+            GetTcpPort();
+
+            UDPServer udpServer = new UDPServer(tcpPort);
+            //TCPServer tcpServer = new TCPServer(tcpPort, localIpListForTcp);
+
+            //odpowiedz od klienta z komunikatem i nr portu
+            udpServer.StartListening();
+
+            udpServer.ListenThrad.Join();
+        }
+
+        private static void GetTcpPort()
+        {
+            Console.WriteLine("Podaj port");
+            do
+            {
+                string tcpPortString = Console.ReadLine();
+                isPortValid = CheckIfPortIsValid(tcpPortString);
+            } while (!isPortValid);
+        }
+
+        private static bool CheckIfPortIsValid(string tcpPortString)
+        {
+            bool isPortValid = true;
+            try
+            {
+                tcpPort = ushort.Parse(tcpPortString);
+            }
+            catch (Exception ex) when (ex is FormatException || ex is ArgumentNullException || ex is OverflowException)
+            {
+                Console.WriteLine("Nastapil blad przy wprowadzaniu portu. Sprobuj ponownie:");
+                isPortValid = false;
+            }
+
+            return isPortValid;
+        }
+
+        private static void GetIpList()
+        {
+            IPAddress[] IpA = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach(var ip in IpA)
+            {
+                if(ip.AddressFamily==AddressFamily.InterNetwork)
+                {
+                    localIpListForTcp.Add(ip.ToString());
+                }
+            }
+
+            localIpListForTcp.Add("127.0.0.1");
         }
     }
 }
---
+++ Server/UDPServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class UDPServer
+    {
+        private ushort tcpPort;
+        private const ushort UDP_PORT = 7;
+        private UdpClient udpClient;
+        private byte[] response;
+        public Thread ListenThrad { get; set; }
+        public ushort TcpPort { get; set; }
+
+        public UDPServer(ushort tcpPort)
+        {
+            SetUdpListener();
+            this.tcpPort = tcpPort;
+        }
+
+        private void SetUdpListener()
+        {
+            try
+            {
+                udpClient = new UdpClient();
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Blad podczas tworzenia socketu, prawdopodobnie port " + UDP_PORT + " moze byc zajety.");
+                Console.WriteLine("Nacisnij dowolny klawisz, aby wyjsc z programu.");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
+        }
+
+        internal void StartListening()
+        {
+            response = Encoding.ASCII.GetBytes("OFFER " + tcpPort);
+
+            //watek odpowiedzialny za nasluchiwanie na udp 
+            ThreadStart udpListenThreadRef = new ThreadStart(Listen);
+        }
+
+        private void Listen()
+        {
+            IPEndPoint clientEp = new IPEndPoint(IPAddress.Any, UDP_PORT);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, UDP_PORT));
+
+            while(true)
+            {
+                Console.WriteLine("Nasluchiwanie na protokole UDP na porcie "+UDP_PORT + "...");
+                byte[] clientRequestData = udpClient.Receive(ref clientEp);
+                string clientRequest = Encoding.ASCII.GetString(clientRequestData);
+                Console.WriteLine("Otrzymano {0} od {1}, wysylanie odpowiedzi", clientRequest, clientEp.Address.ToString());
+                udpClient.Send(response, response.Length, clientEp);
+            }
+        }
+    }
+}
